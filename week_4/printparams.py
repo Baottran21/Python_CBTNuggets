d261188841d7b7cb6312309e36a7c068ce615c2c
Bao Tran <Baottran21@outlook.com>
2023-07-02 20:27:01 -0500

    Print Parameters Sep, End, and File

---
+++ week_4/printparams.py
@@ -0,0 +1,8 @@
+# file
+fileObject = open("output.txt", mode="w")
+print("Hello Bao!", file=fileObject)
+# Sep
+print("Hello", "Bao", "ThirdValue", sep=":")
+# End
+print("Hello", end="")
+print("Goodbye")
