5f67e3a86eb6da4ec49967781d567e2288039200
Bao Tran <Baottran21@outlook.com>
2023-08-17 10:58:25 -0500

    Finished Conditionals Lesson

---
+++ week_6/conditional.py
@@ -0,0 +1,10 @@
+x = 10
+y = 15
+
+if x > y:
+    print("X is in fact, greater than Y.")
+    print("I hope you already knew that")
+    print("Goodbye")
+else:
+    print("Y is greater than X")
+    print("You already knew that")
